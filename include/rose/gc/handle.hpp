3c37bbe83fa4cf02a86a88c94c574eaebdd6ec36
Lian Cheng <rhythm.mail@gmail.com>
2010-08-13 02:38:24 +0800

    Add a simple mark-sweep based non-intrusive garbage collector, which is inspired by the CppGC project owned by Tian Zhongbo.
    - Add gc_suite test suite.
    
    - Extract all functions but main from main.cpp to repl.cpp.

---
+++ include/rose/gc/handle.hpp
@@ -0,0 +1,156 @@
+#ifndef __ROSE_GC_HANDLE_HPP__
+#define __ROSE_GC_HANDLE_HPP__
+
+#include "rose/gc/gc.hpp"
+#include "rose/gc/object.hpp"
+#include "rose/gc/registry.hpp"
+
+#include <boost/logic/tribool.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_convertible.hpp>
+
+namespace rose {
+namespace gc {
+
+class handle_base {
+public:
+    handle_base(object_base* obj) :
+        object_(obj),
+        is_root_(boost::indeterminate)
+    {
+        static int setup_atexit(std::atexit(force_gc));
+        registry::instance().register_handle(this);
+    }
+
+    handle_base(handle_base const& rhs) :
+        object_(rhs.object_)
+    {}
+
+    ~handle_base() {
+        registry::instance().erase_handle(this);
+        object_ = 0;
+    }
+
+    bool operator==(handle_base const& rhs) {
+        return object_ == rhs.object_;
+    }
+
+    bool is_root() const {
+        if (boost::indeterminate(is_root_)) {
+            is_root_ = registry::instance().is_root_handle(this);
+        }
+
+        return is_root_;
+    }
+
+    bool alive() const {
+        return !!object_ ? object_->alive() : false;
+    }
+
+    void alive(bool value) {
+        if (object_)
+            object_->alive(value);
+    }
+
+    std::size_t size() const {
+        return !!object_ ? object_->size() : 0u;
+    }
+
+    object_base* get() {
+        return object_;
+    }
+
+    object_base const* get() const {
+        return object_;
+    }
+
+protected:
+    void copy(handle_base const& rhs) {
+        object_ = rhs.object_;
+    }
+
+private:
+    object_base* object_;
+    mutable boost::tribool is_root_;
+
+};  //  class handle_base
+
+namespace {
+
+struct dummy {
+    void nonnull() {}
+};
+
+typedef void (dummy::*safe_bool)();
+
+}   //  anonymous namespace
+
+template<typename Target>
+class handle : public handle_base {
+public:
+    handle() :
+        handle_base(0)
+    {}
+
+    handle(object<Target>* obj) :
+        handle_base(obj)
+    {}
+
+    handle(handle const& rhs) :
+        handle_base(rhs)
+    {}
+
+    handle& operator=(handle const& rhs) {
+        if (*this == rhs) {
+            return *this;
+        }
+
+        handle_base::copy(rhs);
+        return *this;
+    }
+
+    Target& operator*() {
+        typedef object<Target>* object_ptr;
+        return static_cast<object_ptr>(get())->target();
+    }
+
+    Target const& operator*() const {
+        typedef object<Target>* object_ptr;
+        return static_cast<object_ptr>(get())->target();
+    }
+
+    Target* operator->() {
+        return !!get() ? &(this->operator*()) : 0;
+    }
+
+    Target const* operator->() const {
+        return !!get() ? &(this->operator*()) : 0;
+    }
+
+    bool operator!() const {
+        return !!get();
+    }
+
+    operator safe_bool() const {
+        return !!get() ? &dummy::nonnull : 0;
+    }
+
+    template<typename Other>
+    operator handle<Other>() const {
+        typedef
+            boost::is_convertible<Target, Other>
+            is_convertible;
+
+        BOOST_STATIC_ASSERT(is_convertible::value);
+
+        return handle<Other>(
+                static_cast<object<Other>*>(
+                    const_cast<object_base*>(get())));
+    }
+
+};  //  class handle
+
+}   //  namespace gc
+}   //  namespace rose
+
+#endif  //  __ROSE_GC_HANDLE_HPP__
---
+++ include/rose/gc/registry.hpp
@@ -0,0 +1,114 @@
+#ifndef __ROSE_GC_REGISTRY_HPP__
+#define __ROSE_GC_REGISTRY_HPP__
+
+#include <set>
+
+namespace rose {
+namespace gc {
+
+class handle_base;
+class object_base;
+
+class registry {
+public:
+    typedef unsigned int address_type;
+
+    typedef
+        std::set<address_type>
+        handle_registry;
+
+    typedef
+        std::set<address_type>
+        object_registry;
+
+    typedef
+        std::set<address_type>
+        address_set;
+
+    static registry& instance() {
+        static registry one;
+        return one;
+    }
+
+    void register_handle(handle_base const* handle) {
+        handle_registry_.insert(h2a(handle));
+    }
+
+    void erase_handle(handle_base const* handle) {
+        handle_registry_.erase(h2a(handle));
+    }
+
+    void register_object(object_base const* obj) {
+        object_registry_.insert(o2a(obj));
+    }
+
+    void erase_object(object_base const* obj) {
+        object_registry_.erase(o2a(obj));
+    }
+
+    bool is_root_handle(handle_base const* handle) const;
+
+    void mark();
+
+    void sweep();
+
+    void reset();
+
+private:
+    handle_registry handle_registry_;
+    object_registry object_registry_;
+
+    object_registry::iterator objects_begin() {
+        return object_registry_.begin();
+    }
+
+    object_registry::const_iterator objects_begin() const {
+        return object_registry_.begin();
+    }
+
+    handle_registry::iterator handles_begin() {
+        return handle_registry_.begin();
+    }
+
+    handle_registry::const_iterator handles_begin() const {
+        return handle_registry_.begin();
+    }
+
+    static address_type h2a(handle_base const* handle) {
+        return reinterpret_cast<address_type>(handle);
+    }
+
+    static handle_base* a2h(address_type address) {
+        return reinterpret_cast<handle_base*>(address);
+    }
+
+    static address_type o2a(object_base const* obj) {
+        return reinterpret_cast<address_type>(obj);
+    }
+
+    static object_base* a2o(address_type address) {
+        return reinterpret_cast<object_base*>(address);
+    }
+
+    static bool is_not_root(registry::address_type address);
+
+    static bool is_dead_object(address_type address);
+
+    static bool is_dead_handle(address_type address);
+
+    static bool is_alive_object(address_type address);
+
+    static void delete_dead_object(address_type address);
+
+    static void reset_to_death(address_type address);
+
+    address_set find_root_set() const;
+
+    address_set find_member_handles(handle_base const* handle) const;
+
+};  //  class registry
+
+}   //  namespace gc
+}   //  namespace rose
+
+#endif  //  __ROSE_GC_REGISTRY_HPP__
