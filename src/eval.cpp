1d0fdd008ef8778d601c135750276fb60bdd653a
Lian Cheng <rhythm.mail@gmail.com>
2010-08-17 21:41:56 +0800

    Refactor repl.cpp and eval.cpp.

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,25 +1,46 @@
 #include "rose/eval/evaluator.hpp"
 
+#include <algorithm>
+#include <functional>
+
 namespace rose {
+
+struct evaluate_one :
+    std::binary_function<
+        ast_command_or_definition,
+        environment_ptr,
+        void
+    >
+{
+    mutable gc::handle<value> result;
+
+    void operator()(
+            ast_command_or_definition const& ast,
+            environment_ptr env) const
+    {
+        result = eval(ast, env);
+    }
+
+};  //  struct evaluate_one
 
 gc::handle<value> evaluate_program(
         ast_program const& program,
         environment_ptr env)
 {
     gc::handle<value> return_value;
+    evaluate_one eval;
 
     try {
-        ast_program::const_iterator next;
-
-        for (next = program.begin(); next != program.end(); ++next) {
-            return_value = eval(*next, env);
-        }
+        std::for_each(
+                program.begin(),
+                program.end(),
+                std::bind2nd(eval, env));
     }
     catch(std::exception& e) {
         std::cout << "repl exception: " << e.what() << std::endl;
     }
 
-    return return_value;
+    return eval.result;
 }
 
 }   //  namespace rose
